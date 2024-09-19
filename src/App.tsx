aa568eb40b31e9f16f2af8bb1351f4c211dcc7c8
hmolina <hmolina@cuc.edu.co>
2024-09-19 11:40:57 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import miLogo from "./assets/img/spiderman-png-free.png"
+
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <img src={miLogo} alt="Logo de react" />
+      <h1>Colombia</h1>
+      <p> este es mi primer proyecto en react</p>
+    </div>
+    </>
+  )
+}
+
+export default App
